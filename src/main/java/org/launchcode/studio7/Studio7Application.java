25d268dcca1138ef6a945ca475b943b9dddb85e6
Andie Johnson <ajohnson2@naic.org>
2022-05-02 18:27:42 -0500

    initial commit

---
+++ src/main/java/org/launchcode/studio7/Studio7Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.studio7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studio7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Studio7Application.class, args);
+	}
+
+}
